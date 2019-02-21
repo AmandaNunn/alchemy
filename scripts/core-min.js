f48a272c17a3f0c0ee160a272accc13fc63ff96b
Amanda Nunn <amandanunn@thearrayschool.com>
2019-02-21 08:44:46 -0700

    added hamburger menu functionality updated photos and started the carosel.

---
+++ scripts/core-min.js
@@ -0,0 +1,1 @@
+var mobileNav={mainNav:function(){var e=document.querySelector(".burger"),t=document.querySelector("#"+e.dataset.target);e.addEventListener("click",function(){e.classList.toggle("is-active"),t.classList.toggle("is-active")})}};document.addEventListener("DOMContentLoaded",function(){mobileNav.mainNav()});
--- scripts/core.js
+++ scripts/core.js
@@ -2,4 +2,19 @@
 
 "use strict;"
 
-console.log("JS Test");
+var mobileNav = {
+  mainNav: function(){
+
+    var burger = document.querySelector('.burger');
+    var nav = document.querySelector('#'+burger.dataset.target);
+
+    burger.addEventListener('click', function(){
+      burger.classList.toggle('is-active');
+      nav.classList.toggle('is-active');
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    mobileNav.mainNav();
+  });
