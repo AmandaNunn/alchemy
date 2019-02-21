ef5d2d361be683f1feb822a323aa6f2b6fe9a9cb
Amanda Nunn <amandanunn@thearrayschool.com>
2019-02-21 15:42:28 -0700

    fixed home page and nav bar

--- scripts/core.js
+++ scripts/core.js
@@ -15,6 +15,28 @@ var mobileNav = {
   }
 }
 
+function findYOffset(){
+  var position = window.pageYOffset;
+  if(position >= 40){
+    hideNavBar();
+  } else {
+    showNavBar();
+  }
+}
+
+function showNavBar(){
+  var nav = document.getElementById('nav');
+  nav.classList.remove('is-hidden');
+}
+function hideNavBar(){
+  var nav = document.getElementById('nav');
+  nav.classList.add('is-hidden');
+}
+
+window.addEventListener('scroll', function(){
+  findYOffset();
+});
+
 document.addEventListener("DOMContentLoaded", function(){
-    mobileNav.mainNav();
-  });
+  mobileNav.mainNav();
+});
